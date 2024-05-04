145306ee70270ed70dbd90778c9af4c27c7fd4fb
JRabbit <ledinhtho686@gmail.com>
2024-05-04 18:32:12 +0700

    useState Hook

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './DisplayInfor.scss';
 import logo1 from '../logo.svg';
 
@@ -35,10 +35,20 @@ import logo1 from '../logo.svg';
 
 const DisplayInfor = (props) => {
     const { listUsers } = props;
+    const [isShowHideListUser, setShowHideListUser] = useState(true);
+
+    const handleShowHideListUser = () => {
+        setShowHideListUser(!isShowHideListUser)
+    }
+
     return (
         <div className="display-infor-container">
-
-            {true &&
+            <div style={{ cursor: "pointer" }} onClick={() => handleShowHideListUser()}>
+                <span>
+                    {isShowHideListUser ? " Hide list users:" : "Show list users: "}
+                </span>
+            </div>
+            {isShowHideListUser &&
                 <>
                     {listUsers.map((user, index) => {
                         return (
