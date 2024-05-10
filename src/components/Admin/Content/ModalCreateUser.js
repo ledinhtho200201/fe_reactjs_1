942159d9d4a32f5283a0fceaab0db6e939c2a268
JRabbit <ledinhtho686@gmail.com>
2024-05-10 17:24:59 +0700

    api services and customize axios

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -2,9 +2,8 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { MdFileUpload } from "react-icons/md";
-import axios from 'axios';
 import { toast } from 'react-toastify';
-
+import { postCreateNewUser } from '../../../services/apiServices'
 
 
 const ModalCreateUser = (props) => {
@@ -55,29 +54,20 @@ const ModalCreateUser = (props) => {
             return;
         }
         //submit data & call apis
-        const data = new FormData();
-        data.append('email', email);
-        data.append('password', password);
-        data.append('username', username);
-        data.append('role', role);
-        data.append('userImage', image);
-        let res = await axios.post('http://localhost:8081/api/v1/participant', data)
-        console.log(">>> check res: ", res.data)
-        if (res.data && res.data.EC === 0) {
-            toast.success(res.data.EM);
+        let data = await postCreateNewUser(email, password, username, role, image)
+        // console.log(">>> component res: ", data)
+
+        if (data && data.EC === 0) {
+            toast.success(data.EM);
             handleClose();
         }
-        if (res.data && res.data.EC !== 0) {
-            toast.error(res.data.EM)
+        if (data && data.EC !== 0) {
+            toast.error(data.EM)
         }
     }
 
     return (
         <>
-            {/* <Button variant="primary" onClick={handleShow}>
-                Launch static backdrop modal
-            </Button> */}
-
             <Modal
                 show={show}
                 onHide={handleClose}
